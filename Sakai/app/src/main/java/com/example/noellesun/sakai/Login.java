4c7d1920dcdbbf97af0330b811f103863256205f
yl423 <yl423@duke.edu>
2017-03-26 17:02:06 -0400

    add login

---
+++ Sakai/app/src/main/java/com/example/noellesun/sakai/Login.java
@@ -0,0 +1,27 @@
+package com.example.noellesun.sakai;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser .setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String   failingUrl) {
+
+            }
+        });
+
+        browser.loadUrl("https://sakai.duke.edu");
+
+    }
+}
