1ffacee72aad530491291f9e4c1b529352bef6d7
yz392 <yz392@duke.edu>
2018-03-26 12:12:10 -0400

    web-based login UI to fix time out request error

--- Sakai/app/src/main/java/com/example/noellesun/sakai/Login.java
+++ Sakai/app/src/main/java/com/example/noellesun/sakai/Login.java
@@ -2,16 +2,21 @@ package com.example.noellesun.sakai;
 
 import android.annotation.TargetApi;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -23,7 +28,10 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 
@@ -39,7 +47,6 @@ public class Login extends AppCompatActivity {
     private String TAG = Login.class.getSimpleName();
     String target = "site:";
     String cookiestr;
-    String basic = "https://shib.oit.duke.edu/idp/authn/external?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +56,12 @@ public class Login extends AppCompatActivity {
         WebView browser = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = browser.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        browser.getSettings().setLoadWithOverviewMode(true);
+        browser.getSettings().setUseWideViewPort(true);
         //set cookie to maintain the session
         final CookieManager cookieManager = CookieManager.getInstance();
 
         //Use webview to redirect to sakai login page
-        browser.loadUrl("https://sakai.duke.edu/portal/login");
-        Log.i("url", browser.getUrl());
 
         browser.setWebViewClient(new WebViewClient() {
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -66,17 +73,25 @@ public class Login extends AppCompatActivity {
                     new GetMember().execute();
                 }
             }
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
-                // do your handling codes here, which url is the requested url
-                // probably you need to open that url rather than redirect:
-                if(url.length() >= basic.length() && url.substring(0, basic.length()).equals(basic)) {
-                    Log.e("override url", url);
-                    String summary = generate_login_html(url);
-                    view.loadData(summary, "text/html; charset=utf-8", "utf-8");
+            public void onPageFinished(WebView view, String url){
+                super.onPageFinished(view, url);
+
+                Log.e("on page finishe", url);
+                if(url.contains("https://shib.oit.duke.edu/idp/authn/external?")) {
+//                    view.loadUrl("javascript:"
+//                            + "document.getElementById(\"netid-option\").style.display = 'block';"
+//                    );
+                    view.loadUrl("javascript:(function() { " +
+                            "document.getElementById('netid-option').style.display='block';" +
+                            "document.getElementById('collapse-netid').style.display='none';" +
+                            "document.getElementById('j_username').focus();})()");
                 }
-                return false; // then it is not handled by default action
             }
         });
+
+        browser.loadUrl("https://sakai.duke.edu/portal/logout");
+        browser.loadUrl("https://sakai.duke.edu/portal/login");
+        Log.i("url", browser.getUrl());
 
     }
     //Use AsyncTask to retrieve json from sakai server on the background
