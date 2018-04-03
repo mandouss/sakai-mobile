dd43d356f95a5a26aef4798e5a0ea52fc69fe77d
ny33 <ny33@duke.edu>
2018-04-03 11:10:18 -0400

    optimize

--- Sakai/app/src/main/java/com/example/noellesun/sakai/eachSite.java
+++ Sakai/app/src/main/java/com/example/noellesun/sakai/eachSite.java
@@ -102,18 +102,7 @@ public class eachSite extends AppCompatActivity {
         }
     };
     //redirect to sites view
-    @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data ) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // check that it is the SecondActivity with an OK result
-        if (requestCode == ORDINARY_ACTIVITY_RESULT_CODE) {
-            if (resultCode == 0) {
-                Log.e(TAG, "你进入了Return");
-                finish();
-            }
-        }
-    }
-        final OnClickListener sitesclick = new OnClickListener() {
+    final OnClickListener sitesclick = new OnClickListener() {
         @Override
         public void onClick(View v) {
             finish();
@@ -132,7 +121,7 @@ public class eachSite extends AppCompatActivity {
     };
 
     @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data ) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data ) {
         super.onActivityResult(requestCode, resultCode, data);
         // check that it is the SecondActivity with an OK result
         if (requestCode == ORDINARY_ACTIVITY_RESULT_CODE) {
