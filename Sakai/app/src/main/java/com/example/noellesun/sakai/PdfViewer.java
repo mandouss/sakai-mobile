74342d363096ca210642b7b16f11c1cfffd3db66
Yichuan <ys205@duke.edu>
2018-04-03 13:22:04 -0400

    can use DownloadManager to download a public pdf

--- Sakai/app/src/main/java/com/example/noellesun/sakai/PdfViewer.java
+++ Sakai/app/src/main/java/com/example/noellesun/sakai/PdfViewer.java
@@ -1,5 +1,8 @@
 package com.example.noellesun.sakai;
 
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -10,11 +13,20 @@ public class PdfViewer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.web_layout);
+//        WebView webView = (WebView) findViewById(R.id.webview);
+//        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        String googleDocs = "https://docs.google.com/viewer?url=";
+//        webView.loadUrl(googleDocs + "https://people.duke.edu/~bmr23/ece650/homework/hw1.pdf");
+
         setContentView(R.layout.web_layout);
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
-        webView.getSettings().setJavaScriptEnabled(true);
-        String googleDocs = "https://docs.google.com/viewer?url=";
-        webView.loadUrl(googleDocs + "https://people.duke.edu/~bmr23/ece650/homework/hw1.pdf");
+        DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        String url = "https://people.duke.edu/~bmr23/ece650/homework/hw4.pdf";
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        dm.enqueue(request);
+        Intent i = new Intent();
+        i.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(i);
     }
 }
