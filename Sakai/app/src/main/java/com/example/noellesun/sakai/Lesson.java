97c4b44f628bd811615f564def6dd03809ed66b8
Yichuan <ys205@duke.edu>
2018-04-11 17:10:31 -0400

    in app webview

--- Sakai/app/src/main/java/com/example/noellesun/sakai/Lesson.java
+++ Sakai/app/src/main/java/com/example/noellesun/sakai/Lesson.java
@@ -1,33 +1,33 @@
 package com.example.noellesun.sakai;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Lesson extends AppBaseActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle b = getIntent().getExtras();
         String siteId = b.getString("siteId");
+        Log.i("Lesson: ", siteId);
         String lessonURL = "https://sakai.duke.edu/portal/site/" + siteId + "/tool/1259d18b-8d95-4bd9-9aa8-4a0f3acf6176";
-        setContentView(R.layout.activity_lesson);
-        WebView webView = (WebView) findViewById(R.id.lesson_WebView);
+        setContentView(R.layout.web_layout);
+        WebView webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setPluginState(WebSettings.PluginState.ON);
         webView.getSettings().setJavaScriptEnabled(true);
-        final CookieManager cookieManager = CookieManager.getInstance();
-        String cookiestr = cookieManager.getCookie(lessonURL);
-        Map<String, String> abc = new HashMap<String, String>();
-        abc.put("Cookie", cookiestr);
-        CookieSyncManager.getInstance().sync();
-        webView.loadUrl(lessonURL, abc);
+        webView.loadUrl(lessonURL);
     }
 }
